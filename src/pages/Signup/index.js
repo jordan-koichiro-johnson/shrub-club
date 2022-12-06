675e4a97ed3a1721be99771f5f96a13df558d8b1
Mia <113086524+MiaYun98@users.noreply.github.com>
2022-12-06 13:54:48 -0800

    router setting up for the nav bar

---
+++ src/pages/Signup/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Signup() {
+  return (
+    <div className="Signup">
+        <h1>Signup page! Welcome to my site!</h1>
+        <h2>Isnt react wonderful?</h2>
+    </div>
+  )
+}
