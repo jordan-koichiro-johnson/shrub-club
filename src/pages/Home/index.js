3d985924652022e9277184f1e4f24fdcb6a9ce99
jordan koichiro johnson <mrjordanjohnson@hotmail.com>
2022-12-07 14:01:36 -0800

    working webspeech api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import "./style.css"
+import Voice from './voice/index'
 
 export default function Home({ isLoggedIn }) {
   return (
     <div className="Home">
       {isLoggedIn ? (
-        <div>
+        <div className={'logged'}>
           <h1>shrub generated page!</h1>
+          <Voice />
         </div>) :
         (
-          <div>
+          <div className={'notLogged'}>
             <h1>Welcome to shrub club!</h1>
             <h2>Your Shrub is waiting for you</h2>
+            <Voice />
           </div>
         )}
     </div>
---
+++ src/pages/Home/voice/index.js
@@ -0,0 +1,79 @@
+import React, { useState } from 'react'
+// import { SentimentAnalyzer, stemmer } from 'natural'
+let recording = false
+
+function Voice() {
+    // const analyzer = new SentimentAnalyzer("English", stemmer, "afinn");
+    const [micActive, setMicActive] = useState('Mic')
+    const [value, setValue] = useState('')
+    const [sentiment, setSentiment] = useState('')
+
+    const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+    const recognition = new SpeechRecognition();
+    recognition.addEventListener('start', startSpeechRecognition)
+    recognition.addEventListener('end', endSpeechRecognition)
+    recognition.addEventListener('result', resultSpeechRecognition)
+    function MicButton() {
+        console.log('in onclick')
+        if (recording === false && SpeechRecognition) {
+
+            setMicActive('recording')
+            recognition.start()
+        }
+        if (recording === true && SpeechRecognition) {
+
+            setMicActive('Mic')
+            recognition.stop()
+        }
+        if (!SpeechRecognition) {
+            setMicActive('Mic not working')
+        }
+        recording = !recording
+    }
+
+    function startSpeechRecognition() {
+        console.log('recognizing')
+    }
+    function endSpeechRecognition() {
+        console.log('end recognizing')
+        MicButton()
+    }
+    function resultSpeechRecognition(event) {
+
+        setValue(event.results[0][0].transcript)
+        const str = event.results[0][0].transcript;
+        const stringArray = str.split(/(\s+)/);
+        // setSentiment(analyzer.getSentiment(stringArray))
+        const myInit = {
+            headers: {
+                "Content-Type": "application/json"
+            },
+            method: 'POST',
+            body: JSON.stringify(stringArray),
+
+        };
+        fetch('http://localhost:3006/api/natural/', myInit)
+            .then(res => res.json())
+            .then(res => setSentiment(res))
+            .catch(err => console.log(err))
+    }
+
+
+    return (
+        <div>
+
+            <form>
+                <label for={"talk"}>
+                    Talk:
+                </label>
+
+                <input type={"text"} id={"talk"} name={"talk"} value={value} />
+                <button type={'button'} onClick={MicButton}>{micActive}</button>
+                <button type={'button'}>Submit</button>
+                {sentiment}
+            </form>
+        </div>
+    )
+}
+
+export default Voice
