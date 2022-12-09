9d08d0732ff763361b4e003386c7989b9a07c07c
Mia <113086524+MiaYun98@users.noreply.github.com>
2022-12-08 20:59:46 -0800

    gamemethology is done I think all the stats are changing according to the action that they are doing)

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -161,7 +161,6 @@ export default function Home({ userId, setUserId, isLoggedIn, profileId, setProf
           <div className='notLogged'>
             <h1>Welcome to shrub club!</h1>
             <h2>Your Shrub is waiting for you</h2>
-            { {<Voice />} }
           </div>
         )}
     </div>
